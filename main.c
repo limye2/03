247bbfe121ae21eca24aebf2f0942ac772bb43a0
yerimlee <144992260+limye2@users.noreply.github.com>
2025-09-21 19:42:00 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  03
+//  03 실습2
 //
 //  Created by 이예림 on 9/21/25.
 //
@@ -8,16 +8,14 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    int input_int;
-    float input_float;
+    char c;
+    int i;
     
-    printf("enter an integer : ");
-    scanf("%d", &input_int);
+    printf("input a number : ");
+    scanf("%c", &c);
     
-    printf("enter a float : ");
-    scanf("%f", &input_float);
-    
-    printf("integer : %d, float : %f\n", input_int, input_float);
+    i = c - '0';
+    printf("The input number is %i\n", i);
     
     return 0;
 }
